4fc0a6ef20e15e287948747c9cd8a9f96a635474
kgsuarez23 <61298474+kgsuarez23@users.noreply.github.com>
2020-11-18 10:46:47 -0500

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://25a3f704.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
